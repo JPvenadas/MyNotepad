b229099d82e3df99b0b80d99248ba47e94ee7133
Jp Venadas <jaypheevenadas18@gmail.com>
2022-04-12 12:26:23 +0800

    added async for register

--- screens/Register.jsx
+++ screens/Register.jsx
@@ -1,10 +1,48 @@
-import React from 'react'
-import { View, Text, Button } from 'react-native'
+import React, {useState, useEffect, useLayoutEffect} from 'react'
+import { View, Text, Button, TextInput } from 'react-native'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import { get } from 'react-native/Libraries/TurboModule/TurboModuleRegistry'
+
 
 const Register = ({navigation}) => {
+    const [database, setDatabase] = useState("")
+    
+    const storeData = async () => {
+      const value = {
+        users:[]
+    }
+      try {
+        const jsonValue = JSON.stringify(value)
+        await AsyncStorage.setItem('database', jsonValue)
+        getData()
+      } catch (e) {
+      console.log(e)
+      }
+    }
+
+    const getData = async () => {
+      try {
+        const value = await AsyncStorage.getItem('database')
+        if(value !== null) {
+         setDatabase(JSON.parse(value))
+        }
+        if(value === null){
+         storeData();
+        }
+      } catch(e) {
+       console.log(e)
+      }
+    }
+    
+
+    useEffect(()=>{
+      getData()
+    },[])
+
   return (
-    <View>
+    <View> 
           <Text>Register Screen</Text>
+          
           <Button
               title="Register"
               onPress={() => navigation.navigate('Loading')}
@@ -13,6 +51,8 @@ const Register = ({navigation}) => {
               title="Already have an account?"
               onPress={() => navigation.navigate('Login')}
           />
+           
+           
     </View>
   )
 }
